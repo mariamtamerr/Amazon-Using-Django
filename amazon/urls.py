72b3124ad7327daab898b1c0ece307ea6056ce17
mariamtamerr <memariam5@outlook.com>
2023-10-15 03:32:47 +0300

    Product List View

--- amazon/urls.py
+++ amazon/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 from users import views as user_views
 # from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView, LogoutView
+from pages.views import ProductListView
 
 
 # note : include takes an APP url not a page in an app url
@@ -35,6 +36,7 @@ urlpatterns = [
     path('categories/', include('categories.urls')), 
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+     path('', ProductListView.as_view(), name='home'),
     # path('login/', auth_views.LoginView.as_view('users/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view('users/logout.html'), name='logout'),
 
--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
 from . import views
+from pages.views import ProductListView
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    # path('', ProductListView.as_view(), name='home'),
     path('contact_us', views.contact_us, name='contact-us'),
     path('about_us', views.about_us, name='about-us'),
     path('home_details/<int:product_id>', views.home_details, name='home_details'),
